1f5890bc88a1b8dd87ea2c9d5ef52a70537053aa
github username <github email address>
2019-08-15 18:50:20 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React, { Component } from 'react';
-import {Navbar} from 'react-bootstrap'
+import {Navbar} from 'react-bootstrap';
+import { fetchCats } from './actions/catActions';
+import { connect } from 'react-redux'
+import CatList from './CatList'
 
 class App extends Component {   
+
+  componentDidMount(){
+    if(this.props.cats.length === 0){
+      this.props.fetchCats()
+    }
+  }
   
   render() {
     return (
-      <div className="App">
-        <Navbar>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="#">CatBook</a>
-            </Navbar.Brand>
-          </Navbar.Header>
-        </Navbar>
-      </div>
-    );
+        <div className="App">
+          <Navbar>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <a href="#">CatBook</a>
+                <h1> Hello! </h1>
+              </Navbar.Brand>
+            </Navbar.Header>
+          </Navbar>
+           <CatList catPics={this.props.cats} /> 
+        </div>
+      );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    cats: state.cats.pictures
   }
 }
 
 
 
-export default App
-
+export default connect(mapStateToProps, {fetchCats})(App)
---
+++ src/reducers/cats_reducer.js
@@ -0,0 +1,15 @@
+export default function catsReducer(state = {"loading": false, "pictures": []}, action){
+    switch(action.type){
+
+        case 'LOADING_CATS':
+            return Object.assign({}, ...state, {loading: true})
+
+        case 'FETCH_CATS':
+            console.log("cat reducer!", action.payload)
+            console.log("cat reducer!", state)
+            return {loading: false, pictures: action.payload}
+            
+        default:
+            return state
+    }
+}
